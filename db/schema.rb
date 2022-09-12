a72cf046821003c3cfb29709086927251f0b5852
yuya417 <yk0417yuya@gmail.com>
2022-09-12 12:12:26 +0900

    Branch モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_025153) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_030019) do
+  create_table "branches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "zip_code", null: false
+    t.string "phone_number"
+    t.string "fax_number"
+    t.string "business_hours"
+    t.string "closed_day"
+    t.string "introduction", null: false
+    t.string "after_address", null: false
+    t.bigint "company_id", null: false
+    t.bigint "city_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_branches_on_city_id"
+    t.index ["company_id"], name: "index_branches_on_company_id"
+  end
+
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.bigint "prefecture_id", null: false
@@ -39,5 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_025153) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "branches", "cities"
+  add_foreign_key "branches", "companies"
   add_foreign_key "cities", "prefectures"
 end

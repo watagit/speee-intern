43c66d983dcb3c166382d594942dba931a072acf
yuya417 <yk0417yuya@gmail.com>
2022-09-12 13:55:57 +0900

    BranchとCityの直接の関連付け

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,4 +2,5 @@ class Branch < ApplicationRecord
   belongs_to :company
   has_many :reviews, dependent: :destroy
   has_many :cities, through: :available_areas
+  belongs_to :city
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,6 @@ class City < ApplicationRecord
   has_many :reviews
   belongs_to :prefecture
   has_many :branches, through: :available_areas
+  # TODO: 1つのモデルに対して複数のhas_manyを指定する場合の関連付けを正しくする
+  has_many :branches
 end

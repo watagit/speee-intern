1d33a982d080ea46443b57d19da668227cc0f6d4
yuya417 <yk0417yuya@gmail.com>
2022-09-12 11:58:15 +0900

    City モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_024717) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_025153) do
+  create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "prefecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_cities_on_prefecture_id"
+  end
+
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
@@ -31,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_024717) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cities", "prefectures"
 end

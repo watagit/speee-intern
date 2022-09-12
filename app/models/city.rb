24b74d59a4f7176a45fb35f3fb1f1c4ff64caa0c
yuya417 <yk0417yuya@gmail.com>
2022-09-12 14:17:25 +0900

    BranchとCityの関連付けの修正

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ApplicationRecord
-  has_many :reviews
   belongs_to :prefecture
+  has_many :reviews
+  has_many :available_areas
   # 中間テーブルを介した関連付け
   has_many :branches, class_name: "Branch", through: :available_areas
   # 中間テーブルを介さない関連付け

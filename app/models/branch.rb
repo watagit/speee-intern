35c47bcd218143841f0fa9042f111ad8f2538b43
yuya417 <yk0417yuya@gmail.com>
2022-09-12 13:49:34 +0900

    BranchとCityのリレーションの追加

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,5 @@
 class Branch < ApplicationRecord
   belongs_to :company
   has_many :reviews, dependent: :destroy
+  has_many :cities, through: :available_areas
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,5 @@
 class City < ApplicationRecord
   has_many :reviews
   belongs_to :prefecture
+  has_many :branches, through: :available_areas
 end

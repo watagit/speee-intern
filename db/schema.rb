0339a84cc6e99e06f5ffb9d8c1ba0d865d60fd63
yuya417 <yk0417yuya@gmail.com>
2022-09-12 11:50:19 +0900

    PropertyTypeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_024225) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_024717) do
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_024225) do
   end
 
   create_table "prefectures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "property_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

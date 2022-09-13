879f75734a97d882cf775d893442a2754bfa0ebb
watagit <wtr0414@gmail.com>
2022-09-13 17:31:09 +0900

    wip: post用のメソッド作成の途中

--- app/helpers/assessment_forms_helper.rb
+++ app/helpers/assessment_forms_helper.rb
@@ -1,2 +1,6 @@
 module AssessmentFormsHelper
+  def post_assessment(params)
+    uri = URI.parse('https://miniul-api.herokuapp.com//affiliate/v2/conversions')
+    response = Net::HTTP.post(uri, params)
+  end
 end

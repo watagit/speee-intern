379c1c6ff2733d5c727ab1a3916dd821a4f0110c
watagit <wtr0414@gmail.com>
2022-09-13 14:12:00 +0900

    BranchとAvailableAreaの依存関係の修正

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,7 +1,7 @@
 class Branch < ApplicationRecord
   belongs_to :company
   belongs_to :city
-  has_many :available_areas, dependent: :restrict_with_exception
+  has_many :available_areas, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :cities, through: :available_areas
 end

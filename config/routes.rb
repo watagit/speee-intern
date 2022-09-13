4f7cc177ac605ad0daeff7055052038a827da8bf
mono-glyceride <glyceride.mono@gmail.com>
2022-09-13 16:28:05 +0900

    フォームのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/ops/heartbeat', to: 'health_check#index'
 
   get '/branches/:id', to: 'branches#show'
+  resources :assessment_forms, only:[ :new, :create ]
 end

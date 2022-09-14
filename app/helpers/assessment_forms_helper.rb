ff1e87fd284dc7353403a97703579bd844ca288d
watagit <wtr0414@gmail.com>
2022-09-14 11:41:06 +0900

    API通信用のメソッドの作成

--- app/helpers/assessment_forms_helper.rb
+++ app/helpers/assessment_forms_helper.rb
@@ -1,6 +1,12 @@
+require 'net/http'
+
 module AssessmentFormsHelper
+  # 査定依頼情報をイエウールに送信する
+  # 通信に成功したらtrueを返す
   def post_assessment(params)
-    uri = URI.parse('https://miniul-api.herokuapp.com//affiliate/v2/conversions')
-    response = Net::HTTP.post(uri, params)
+    uri = URI.parse('https://miniul-api.herokuapp.com/affiliate/v2/conversions')
+    response = Net::HTTP.post_form(uri, params)
+
+    response.code == '200'
   end
 end

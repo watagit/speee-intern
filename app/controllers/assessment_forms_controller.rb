99c307095c75f32fec44b2a90f970da979980982
mono-glyceride <glyceride.mono@gmail.com>
2022-09-14 15:17:19 +0900

    rubocopで自動修正

--- app/controllers/assessment_forms_controller.rb
+++ app/controllers/assessment_forms_controller.rb
@@ -9,33 +9,33 @@ class AssessmentFormsController < ApplicationController
       redirect_to thanks_path
     else
       flash.now[:alert] = '査定情報の送信に失敗しました'
-      render action: :new, status: 422
+      render action: :new, status: :unprocessable_entity
     end
     # rubocop:enable Rails/I18nLocaleTexts
   end
 
   private
 
-    def assessment_form_params
-      params.permit(
-        :branch_id,
-        :prefecture_id,
-        :city_id,
-        :property_type,
-        :property_exclusive_area,
-        :property_land_area,
-        :property_building_area,
-        :property_building_area_unit,
-        :property_floor_area,
-        :property_room_plan,
-        :property_constructed_year,
-        :property_address,
-        :user_email,
-        :first_name,
-        :last_name,
-        :first_name_kana,
-        :last_name_kana,
-        :user_tel
-      )
+  def assessment_form_params
+    params.permit(
+      :branch_id,
+      :prefecture_id,
+      :city_id,
+      :property_type,
+      :property_exclusive_area,
+      :property_land_area,
+      :property_building_area,
+      :property_building_area_unit,
+      :property_floor_area,
+      :property_room_plan,
+      :property_constructed_year,
+      :property_address,
+      :user_email,
+      :first_name,
+      :last_name,
+      :first_name_kana,
+      :last_name_kana,
+      :user_tel
+    )
   end
 end

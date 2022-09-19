a6f6293997009940c3b762a6f28e6e096400d050
Hayato-7812 <haya12to17@gmail.com>
2022-09-19 17:13:50 +0900

    簡潔な記法に変更

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
-    @prefecture = Prefecture.find_by(id: @city.prefecture_id)
+    @prefecture = @city.prefecture
     branch_ids = AvailableArea.where(city_id: @city.id).pluck(:branch_id)
     @city_branches = Branch.where(id: branch_ids)
   end

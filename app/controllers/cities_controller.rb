3e94bce7e80dc55ddd3f55e7f246d59a996b020e
Hayato-7812 <haya12to17@gmail.com>
2022-09-19 16:56:51 +0900

    22_市区町村ページの作成

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,8 @@
+class CitiesController < ApplicationController
+  def show 
+    @city = City.find(params[:id])
+    @prefecture = Prefecture.find_by(id: @city.prefecture_id)
+    branch_ids = AvailableArea.where(city_id: @city.id).pluck(:branch_id)
+    @city_branches = Branch.where(id: branch_ids)
+  end 
+end

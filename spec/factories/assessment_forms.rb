14836c12d95508cdabcd847e54f82f89489ee082
mono-glyceride <glyceride.mono@gmail.com>
2022-09-20 11:22:38 +0900

    fackerを用いてfactorybotのデータを生成

--- spec/factories/assessment_forms.rb
+++ spec/factories/assessment_forms.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :assessment_form do
-    ieul_branch_id { 1 }
-    property_city { 264 }
-    property_type { 4 }
-    property_exclusive_area { 10 }
-    property_land_area { 20 }
-    property_building_area { 10 }
-    property_building_area_unit { 0 }
-    property_floor_area { 8 }
-    property_room_plan { 1 }
-    property_constructed_year { 35 }
-    property_address { '連坊一丁目4-1' }
-    user_email { 'nika@example.com' }
-    first_name { '二華子' }
-    last_name { '仙台' }
+    ieul_branch_id { Faker::Number.between(from: 0, to: 1024) }
+    property_city { Faker::Number.between(from: 0, to: 1899) }
+    property_type { Faker::Number.between(from: 1, to: 3) }
+    property_exclusive_area { Faker::Number.between(from: 0, to: 1000) }
+    property_land_area { Faker::Number.between(from: 0, to: 1000) }
+    property_building_area { Faker::Number.between(from: 0, to: 1000) }
+    property_building_area_unit { Faker::Number.between(from: 1, to: 2) }
+    property_floor_area { Faker::Number.between(from: 0, to: 1000) }
+    property_room_plan { Faker::Number.between(from: 0, to: 13) }
+    property_constructed_year { Faker::Number.between(from: 0, to: 100) }
+    property_address { Faker::Lorem.sentence(word_count: 5) }
+    user_email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     first_name_kana { 'にかこ' }
     last_name_kana { 'せんだい' }
     user_tel { '0000000000' }

c0b2237aebf5c3a0a6fc6d5b829f06faee7ad228
watagit <wtr0414@gmail.com>
2022-09-20 14:44:02 +0900

    テストしないため削除

--- spec/controllers/branches_controller_spec.rb
+++ spec/controllers/branches_controller_spec.rb
@@ -6,9 +6,5 @@ RSpec.describe 'Branches', type: :request do
       get branch_path branch
       expect(response).to have_http_status(:success)
     end
-
-    it 'branchがない場合500が返る' do
-      # TODO: controller側で404を返すようになってから実装する
-    end
   end
 end

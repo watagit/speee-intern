c93c86728fc42bff3c7ba7fb13138a3834cf3989
watagit <wtr0414@gmail.com>
2022-09-20 14:56:18 +0900

    ReviewのFactoryBot定義

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,33 @@
+# rubocop:disable Metrics/BlockLength
+FactoryBot.define do
+  factory :review do
+    name { Faker::Lorem.sentence(word_count: 3) }
+    gender { '男性' }
+    age { Faker::Number.number }
+    sell_count { '初めて' }
+    considering_selling_at { '2022-09-20' }
+    assessment_requested_at { '2022-09-20' }
+    sales_started_at { '2022-09-20' }
+    sold_at { '2022-09-20' }
+    delivered_at { '2022-09-20' }
+    assessment_price { Faker::Number.number }
+    sold_price { Faker::Number.number }
+    is_discount { 0 }
+    discount_date_type_after_sales_started { 1 }
+    brokerage_contract_type { 1 }
+    headline { Faker::Lorem.sentence(word_count: 2) }
+    reason_for_sale { 0 }
+    comment_anxiety { Faker::Lorem.sentence(word_count: 10) }
+    comment_reason_to_decide { Faker::Lorem.sentence(word_count: 10) }
+    satisfaction_level { 5 }
+    comment_reason_for_satisfaction { Faker::Lorem.sentence(word_count: 10) }
+    advice { Faker::Lorem.sentence(word_count: 10) }
+    contract_price { Faker::Number.number }
+    review_digest { Faker::Lorem.sentence(word_count: 1) }
+
+    association :property_type
+    association :city
+    association :branch
+  end
+end
+# rubocop:enable Metrics/BlockLength

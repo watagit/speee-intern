fbb8a1f0c061fa186f50083eeccae46a996a1ddd
Hayato-7812 <haya12to17@gmail.com>
2022-09-21 14:32:50 +0900

    見た目の修正done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def show
-    @prefectures = Prefecture.all
+    @hokkaidotohoku= Prefecture.where(id:1..7)
+    @kanto = Prefecture.where(id:8..14)
+    @chubu = Prefecture.where(id:15..23)
+    @kinki = Prefecture.where(id:24..30)
+    @chugoku = Prefecture.where(id:31..35)
+    @shikoku = Prefecture.where(id:36..39)
+    @kyusyuokinawa = Prefecture.where(id:40..47)
   end
 end

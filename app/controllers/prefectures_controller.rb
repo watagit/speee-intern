5eb826565d146bdaec25fccbf10da075b3f91bac
watagit <wtr0414@gmail.com>
2022-09-21 11:11:54 +0900

    将来的に修正が必要な箇所にコメントを追加

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -3,6 +3,8 @@ class PrefecturesController < ApplicationController
     @prefecture = Prefecture.find(params[:id])
 
     branch_ids = AvailableArea.where(city_id: @prefecture.cities.ids).pluck(:branch_id)
+    
+    # HACK: 店舗が増えると処理も重くなるため、ゆくゆくは定期実行->カラム更新などで対応したい
     @prefecture_branches = Branch.where(id: branch_ids).eager_load(:reviews, :company, city: [:prefecture]).sort do
       |a, b| b.average_satisfaction_level <=> a.average_satisfaction_level
     end
